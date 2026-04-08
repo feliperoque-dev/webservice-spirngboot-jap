81db580cc7ca61415f303d3b85e2510c7890c607
Felipe Roque <felipegrroque@gmail.com>
2026-04-08 18:27:01 -0300

    User entity and recourse

---
+++ src/main/java/com/webservice/course/recources/UserResource.java
@@ -0,0 +1,20 @@
+package com.webservice.course.recources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webservice.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Felipe", "felipe@gmail.com", "77999676767", "12345678");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
