9c15b9ba60c468d14b1279473a202145aa754e9e
Felipe Roque <felipegrroque@gmail.com>
2026-05-14 17:28:43 -0300

    OrderStatus enum

--- src/main/java/com/webservice/course/config/TestConfig.java
+++ src/main/java/com/webservice/course/config/TestConfig.java
@@ -10,33 +10,33 @@ import org.springframework.context.annotation.Profile;
 
 import com.webservice.course.entities.Order;
 import com.webservice.course.entities.User;
+import com.webservice.course.entities.enums.OrderStatus;
 import com.webservice.course.repositories.OrderRepository;
 import com.webservice.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner{
-	
+public class TestConfig implements CommandLineRunner {
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		User u1 = new User(null,"Felipe", "felipe@gmail.com", "77999676767", "12345678");
-		User u2 = new User(null,"Maria", "maria@gmail.com", "77999696969", "12345678");
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-		
-		userRepository.saveAll(Arrays.asList(u1,u2));
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
-		
+
+		User u1 = new User(null, "Felipe", "felipe@gmail.com", "77999676767", "12345678");
+		User u2 = new User(null, "Maria", "maria@gmail.com", "77999696969", "12345678");
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAINTING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAINTING_PAYMENT, u1);
+
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
 	}
-	
-		
+
 }
