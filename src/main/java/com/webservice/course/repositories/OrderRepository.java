9b2c3f2e222815a0201a1fe1ca7767e5d586d6cf
Felipe Roque <felipegrroque@gmail.com>
2026-05-14 15:53:56 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/webservice/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.webservice.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webservice.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
