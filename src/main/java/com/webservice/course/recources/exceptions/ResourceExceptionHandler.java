68c696ccdce545dc70d7832690b4b477124176bc
Felipe Roque <felipegrroque@gmail.com>
2026-05-22 11:35:46 -0300

    Exception handling - delete

--- src/main/java/com/webservice/course/recources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/webservice/course/recources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.webservice.course.services.exceptions.DatabaseException;
 import com.webservice.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,9 +23,14 @@ public class ResourceExceptionHandler implements Serializable {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(), String.valueOf(status), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
-		
 	}
 	
-	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), String.valueOf(status), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 	
 }
